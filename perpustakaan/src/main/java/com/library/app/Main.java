a885f587541ba9ce63fc0d8b89a4ad1dd90b10e9
Muhammad Adha <muhammadadha153@gmail.com>
2026-04-17 10:46:04 +0700

    buat frame utama untukk kiosk

--- perpustakaan/src/main/java/com/library/app/Main.java
+++ perpustakaan/src/main/java/com/library/app/Main.java
@@ -1,11 +1,12 @@
 package com.library.app;
 
 import com.library.app.bootstrap.DatabaseInitializer;
-import com.library.app.ui.fx.LibraryFxApp;
 
 public class Main {
     public static void main(String[] args) {
         DatabaseInitializer.initialize();
-        javafx.application.Application.launch(LibraryFxApp.class, args);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new com.library.app.ui.LoginFrame().setVisible(true);
+        });
     }
 }
